e793479c3397ac5caf3ca3beff43c55d43e2da52
Pablo Porzio <pabloporzio97@gmail.com>
2024-10-31 13:08:08 -0300

    Initial Commit

---
+++ OnboardingFlow/OnboardingFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardingFlowApp.swift
+//  OnboardingFlow
+//
+//  Created by Pablo Porzio (Modak) on 31/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct OnboardingFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
